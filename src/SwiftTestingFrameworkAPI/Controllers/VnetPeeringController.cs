9509b1de0798168ed4d2c5bf80f2e4057fa6fa9d
Xingyang Li <xingyangli@microsoft.com>
2023-01-13 11:10:52 -0800

    testing changes

---
+++ src/SwiftTestingFrameworkAPI/Controllers/VnetPeeringController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using SwiftTestingFrameworkAPI.Utils;
+
+namespace SwiftTestingFrameworkAPI.Controllers
+{
+    [ApiController]
+    [Route("PingPeeredVM")]
+    public class VnetPeeringController : ControllerBase
+    {
+
+        private readonly ILogger<VnetPeeringController> _logger;
+
+        private const string TestName = "PingPeeredVM";
+
+        public VnetPeeringController(ILogger<VnetPeeringController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public TestResponse GetInfo()
+        {
+            string testDetails = "Send TCP ping to VM that is in a peered VNET";
+            return new TestResponse(Constants.ApiVersion, TestName, string.Empty, testDetails, string.Empty);
+        }
+
+        [HttpPost]
+        public TestResponse PeerVM()
+        {
+            Helper.ProcessOutput p;
+            try
+            {
+
+                if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                {
+                    p = Helper.StartProcess("tcpping.exe", Constants.PeeredVmAddress);
+                }
+                else
+                {
+                    p = Helper.StartProcess("curl", Constants.PeeredVmAddress);
+                }
+
+                if (p.ExitCode == 0)
+                {
+                    return new TestResponse(Constants.ApiVersion, TestName, "Success", p.StdOutput, string.Empty);
+                }
+                else
+                {
+                    return new TestResponse(Constants.ApiVersion, TestName, "Failure", string.Empty, p.StdError);
+                }
+            }
+            catch (Exception ex)
+            {
+                return new TestResponse(Constants.ApiVersion, TestName, "Failure", string.Empty, ex.Message + ex.StackTrace);
+            }
+        }
+    }
+}
--- src/SwiftTestingFrameworkAPI/Utils/Constants.cs
+++ src/SwiftTestingFrameworkAPI/Utils/Constants.cs
@@ -5,6 +5,7 @@
         public const string ApiVersion = "1.0";
 
         public const string VmAddress = "10.0.0.4";
+        public const string PeeredVmAddress = "172.16.0.4";
         public const string AzureDNS = "168.63.129.16";
         public const string StorageContainerName = "testcontainer";
         public const string StorageConnectionStringName = "CUSTOMCONNSTR_Storage";
